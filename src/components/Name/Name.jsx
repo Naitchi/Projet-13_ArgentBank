56b7dd6a0a0f07b4a2aa7bca80b6610bc51a1a52
Naitchi <assasindu39@gmail.com>
2023-04-23 20:24:00 +0200

    put all the code in user into a new component to make the page user more clean

---
+++ src/components/Name/Name.jsx
@@ -0,0 +1,103 @@
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+
+// Services
+import { getProfile, modifyProfile } from '../../services/user.service';
+
+// Store
+import { useDispatch, useSelector } from 'react-redux';
+import { setToken } from '../../store/slices/authSlice';
+import { setUser } from '../../store/slices/userSlice';
+
+// Css
+import styles from './Name.module.css';
+
+export default function Name() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+
+  const [state, setState] = useState({ user: {}, edit: false, newUser: {} });
+  let token = useSelector((state) => state.auth.token);
+
+  const changeEdit = () => {
+    setState({ ...state, edit: !state.edit });
+  };
+
+  const modifyUser = async () => {
+    const response = await modifyProfile(state.newUser, token);
+    if (response === true)
+      setState({
+        ...state,
+        edit: false,
+        user: {
+          ...state.user,
+          firstName: state.newUser.firstName,
+          lastName: state.newUser.lastName,
+        },
+      });
+  };
+
+  useEffect(() => {
+    const getUser = async () => {
+      if (token) {
+        const data = await getProfile(token);
+        setState({ ...state, user: data });
+        dispatch(setUser(data));
+      }
+    };
+    if (!token && typeof window !== 'undefined') {
+      token = JSON.parse(localStorage.getItem('token'));
+      if (!token) router.push('/login');
+      else dispatch(setToken(token));
+    }
+    getUser();
+  }, [dispatch, router, token]);
+
+  return (
+    <div className={styles.top}>
+      <p>Welcome back</p>
+      {state.user && !state.edit && (
+        <p>
+          {state.user.firstName} {state.user.lastName} !
+        </p>
+      )}
+      {state.user && state.edit && (
+        <div>
+          <div>
+            <input
+              className={styles.input}
+              placeholder={state.user.firstName}
+              onChange={(event) =>
+                setState({
+                  ...state,
+                  newUser: { ...state.newUser, firstName: event.target.value },
+                })
+              }
+            />
+            <input
+              className={styles.input}
+              placeholder={state.user.lastName}
+              onChange={(event) =>
+                setState({
+                  ...state,
+                  newUser: { ...state.newUser, lastName: event.target.value },
+                })
+              }
+            />
+          </div>
+          <button className={`${styles.edit} ${styles.save}`} onClick={modifyUser}>
+            Save
+          </button>
+          <button className={styles.edit} onClick={changeEdit}>
+            Cancel
+          </button>
+        </div>
+      )}
+      {state.user && !state.edit && (
+        <button className={styles.edit} onClick={changeEdit}>
+          Edit Name
+        </button>
+      )}
+    </div>
+  );
+}
--- src/pages/user.js
+++ src/pages/user.js
@@ -1,113 +1,21 @@
-import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+
 
 // Components
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import Account from '../components/Account/Account';
+import Name from '../components/Name/Name';
 
 // Css
 import styles from '../styles/User.module.css';
 
-// Services
-import { getProfile, modifyProfile } from '../services/user.service';
-
-// Store
-import { useDispatch, useSelector } from 'react-redux';
-import { setToken } from '../store/slices/authSlice';
-import { setUser } from '../store/slices/userSlice';
 
 export default function User() {
-  const dispatch = useDispatch();
-  const router = useRouter();
-
-  const [state, setState] = useState({ user: {}, edit: false, newUser: {} });
-  let token = useSelector((state) => state.auth.token);
-
-  const changeEdit = () => {
-    setState({ ...state, edit: !state.edit });
-  };
-
-  const modifyUser = async () => {
-    const response = await modifyProfile(state.newUser, token);
-    if (response === true)
-      setState({
-        ...state,
-        edit: false,
-        user: {
-          ...state.user,
-          firstName: state.newUser.firstName,
-          lastName: state.newUser.lastName,
-        },
-      });
-  };
-
-  useEffect(() => {
-    const getUser = async () => {
-      if (token) {
-        const data = await getProfile(token);
-        setState({ ...state, user: data });
-        dispatch(setUser(data));
-      }
-    };
-    if (!token && typeof window !== 'undefined') {
-      token = JSON.parse(localStorage.getItem('token'));
-      if (!token) router.push('/login');
-      else dispatch(setToken(token));
-    }
-    getUser();
-  }, [dispatch, router, token]);
-
   return (
     <div className={styles.Connect}>
       <Header />
-
       <div className={styles.box}>
-        <div className={styles.top}>
-          <p>Welcome back</p>
-          {state.user && !state.edit && (
-            <p>
-              {state.user.firstName} {state.user.lastName} !
-            </p>
-          )}
-          {state.user && state.edit && (
-            <div>
-              <div>
-                <input
-                  className={styles.input}
-                  placeholder={state.user.firstName}
-                  onChange={(event) =>
-                    setState({
-                      ...state,
-                      newUser: { ...state.newUser, firstName: event.target.value },
-                    })
-                  }
-                />
-                <input
-                  className={styles.input}
-                  placeholder={state.user.lastName}
-                  onChange={(event) =>
-                    setState({
-                      ...state,
-                      newUser: { ...state.newUser, lastName: event.target.value },
-                    })
-                  }
-                />
-              </div>
-              <button className={`${styles.edit} ${styles.save}`} onClick={modifyUser}>
-                Save
-              </button>
-              <button className={styles.edit} onClick={changeEdit}>
-                Cancel
-              </button>
-            </div>
-          )}
-          {state.user && !state.edit && (
-            <button className={styles.edit} onClick={changeEdit}>
-              Edit Name
-            </button>
-          )}
-        </div>
+        <Name />
         <Account name="Checking" id="8349" count="2,082.79" statut="Available" />
         <Account name="Savings" id="6712" count="10,928.42" statut="Available" />
         <Account name="Credit Card" id="8349" count="184.30" statut="Current" />
